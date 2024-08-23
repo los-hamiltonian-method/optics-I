b90a82bb7fb69e9cb6d08f957d46da2e3cd59047
VerthObserver <emilio.moreno@ciencias.unam.mx>
2024-08-22 21:39:24 -0600

    wtf is happening

--- Práctica 1/Datos/analysis_n.py
+++ Práctica 1/Datos/analysis_n.py
@@ -2,11 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+# Lente positivo
 data = pd.read_csv('len_pos.csv')
-dist_focales = (data['cota_i'] + data['cota_d'])/2
-errors = data['cota_i'] - data['cota_d']
-dist_focales = dist_focales + 2
-print(f"{np.mean(dist_focales) + 2.9} +/- {np.std(errors) + 0.1}")
+f1s = (data['cota_i'] + data['cota_d']) / 2
+errors_f1 = data['cota_i'] - data['cota_d']
+# Olvidamos considerar 2cm
+f1s = f1s + 2.9
+print(f"{np.mean(f1s)} +/- {np.std(errors_f1) + 0.1}")
 
 
-#Lente compuesto D = 58.4
+# Lente compuesto
+def calculate_f2(bfl, f1, d):
+	return (bfl * (f1 - d)) / (bfl + f1 - d)
+
+
+def calculate_f2_ffl(ffl, f1, d):
+	return (f1 * (d + ffl) - ffl * d) / (f1 - ffl)
+
+
+data2 = pd.read_csv('len_compuesto.csv')
+# b es back focal length (bfl)
+bs = (data2['b_i'] + data2['b_d']) / 2
+# Distancia entre lentes
+ds = data2['b+d'] - bs
+# Diámetros de la imagen
+diams = data2['diam']
+
+print(calculate_f2(bs.mean(), f1s.mean(), ds.mean()))
+print(calculate_f2_ffl(bs.mean(), f1s.mean(), ds.mean()))
+
+
+# Lente compuesto D = 58.4
